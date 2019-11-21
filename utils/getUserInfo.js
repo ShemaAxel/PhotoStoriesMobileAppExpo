fe97557e0b6486d60649967b57e2c828f8c4d19c
axel <shemaromeoaxel@gmail.com>
2019-11-21 12:17:59 +0200

    Works perfectly now...git add README.md

---
+++ utils/getUserInfo.js
@@ -0,0 +1,12 @@
+// import { Constants } from "expo";
+import Constants from "expo-constants";
+
+function getUserInfo() {
+  const { deviceId, deviceName, platform } = Constants;
+  return {
+    deviceId,
+    deviceName,
+    platform
+  };
+}
+export default getUserInfo;
